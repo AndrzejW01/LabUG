225fd9a5b02d3f3d6b2875a33f0d66df9680bfe4
Andrzej Walewski <a.walewski.349@studms.ug.edu.pl>
2025-11-23 14:01:08 +0100

    zadanie 1 oraz 2

---
+++ zadanie2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int potega(int a, int b) {
+    int wynik = 1;
+    for (int i = 1; i <= b; i++) {
+        wynik *= a;  // Mnożymy wynik przez a w każdej iteracji
+    }
+    return wynik;
+}
+
+int main() {
+    int a, b;
+    
+    printf("Podaj liczbę naturalną a: ");
+    scanf("%d", &a);
+    
+    printf("Podaj liczbę naturalną b: ");
+    scanf("%d", &b);
+    
+    if (b < 0) {
+        printf("Potęga nie może być liczbą ujemną.\n");
+    } else {
+        printf("%d do potęgi %d wynosi: %d\n", a, b, potega(a, b));
+    }
+    
+    return 0;
+}
